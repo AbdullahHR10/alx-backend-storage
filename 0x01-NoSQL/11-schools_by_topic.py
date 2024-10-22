c89f9fdaa7e8aa4c456f845f69fdc6c7c1bdeca3
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-10-22 15:12:09 +0300

    Add schools_by_topic function

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module that contains schools_by_topic function. """
+
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ Function that returns the list of school having a specific topic. """
+    filtered_topics = {"topics": "topics"}
+    return list(mongo_collection.find(filtered_topics))
